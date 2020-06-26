abf802095c14204286e2bb70e363a79b1edb8b1e
wineye <mistwineye@hotmail.com>
2020-06-27 03:05:21 +0800

    generate mybatis files

---
+++ infohub/src/main/java/com/hiyichang/infohub/InfoHubApplication.java
@@ -0,0 +1,13 @@
+package com.hiyichang.infohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoHubApplication.class, args);
+	}
+
+}
